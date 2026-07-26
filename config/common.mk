05b18e54f9e50ddce5ec0ee0c773910c48ca8f0c
neophyte404 <260553983+neophyte404@users.noreply.github.com>
2026-07-26 04:32:04 +0000

    lineage: Add GMS Support

--- config/common.mk
+++ config/common.mk
@@ -5,6 +5,15 @@ PRODUCT_BRAND ?= LineageOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+# GMS
+ifeq ($(WITH_GMS), true)
+LINEAGE_ZIP_TYPE := CORE
+LINEAGE_CORE := true
+$(call inherit-product, vendor/gms/common/common-vendor.mk)
+else
+LINEAGE_ZIP_TYPE := VANILLA
+endif
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
